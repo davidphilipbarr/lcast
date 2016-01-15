e779a528279d8d47667f37b7eca8e8d05e293e65
David <david@gihon>
2016-01-15 23:24:07 +0000

    first and maybe last go at this

---
+++ README.md
@@ -0,0 +1,1 @@
+Turn off or dim your lightbulbs when cast is active usuing shell and only basic stuff you might find on maybe raspbian and maybe try not to use the lifx api on day, fuck that shit keep it local. 
---
+++ lcast.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+KEY=$(echo "YOUR KEY");
+LIFX="https://api.lifx.com/v1/lights";
+CASTSTATUS=$(avahi-browse -rt _googlecast._tcp | grep txt | cut -d'"' -f 6 | cut -d'=' -f 2);
+LFXS=$(cat /tmp/lifxstat | cut -d'|' -f 1);
+BULB0='all';
+
+
+echo "$CASTSTATUS"  >> ~/.caststatus;
+
+CHANGE=$(tail -n 2 ~/.caststatus | tr '\n' ' ' | sed 's/ //')
+
+lcaston()
+{
+curl -s -u "$KEY" -X PUT -d "power=$LFXS2" "$LIFX/$BULB0/state";
+}
+
+lcastoff()
+{
+curl -s -u "$KEY" -X PUT -d "power=off" "$LIFX/$BULB0/state";
+}
+
+lifxst()
+{
+LIFXSTATUS2=$(curl -s -u "$KEY" "$LIFX/$BULB0" | grep power | cut -d '"'  -f 4 | head -1);
+echo "$LIFXSTATUS|$LIFXSTATUS2" > /tmp/lifxstat;
+}
+
+if [ $CHANGE = "01" ];
+then
+lifxst && lcastoff
+elif [ $CHANGE = "10" ];
+then 
+lcaston
+
+else 
+echo "DOING NOTHING";
+fi
+
